994620d2620106ccf8bce5ad47aa92765f13e1d2
Waramidango <hirotodayo060630@gmail.com>
2026-09-10 14:17:32 +0900

    Complete

--- src/vec3.h
+++ src/vec3.h
@@ -63,6 +63,11 @@ class vec3 {
             return (std::fabs(e[0]) < s) && (std::fabs(e[1] < s)) && (std::fabs(e[2]) < s);
         }
 
+        static vec3 random() {
+            return vec3(random_double(), random_double(), random_double());
+        }
+
+
         static vec3 random(double min, double max) {
             return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
         }
@@ -123,6 +128,14 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 
 inline vec3 unit_vector(const vec3& v) { // 単位ベクトル
     return v / v.length();
+}
+
+inline vec3 random_in_unit_disk(){
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
+        if (p.length_squared() < 1)
+            return p;
+    }
 }
 
 inline vec3 random_unit_vector() {
